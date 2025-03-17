92ce8223a5e58997432d707e2c189edb104178b2
Guilherme Alves <Guilhermealves53139@gmail.com>
2025-03-17 20:32:08 -0300

    primeiro commit

---
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconComponent ,FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+
+
+
+
+@Component({
+  imports: [RouterModule, FaIconComponent],
+  selector: 'ecom-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit{
+
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+      this.initFontAwesome();
+  }
+
+  private initFontAwesome(){
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+}
